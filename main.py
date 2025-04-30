9317a21fd21454fc88831ca239dd2cf8df51cd24
Boychik8787 <boychik8787@gmail.com>
2025-04-30 15:02:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=["start"])
 async def start(msg: types.Message):
     await msg.answer("Привет! Отправь мне дату, время и место рождения, чтобы я построил твою натальную карту.")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True)
+Fix bot
